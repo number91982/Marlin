4518a34ab3e90a7d1a1309b1b8bf7db483a1896d
number91982 <37806858+number91982@users.noreply.github.com>
2018-03-26 16:53:08 +0200

    Update pins_RADDS.h
    Fixed compiling issues

--- Marlin/src/pins/pins_RADDS.h
+++ Marlin/src/pins/pins_RADDS.h
@@ -140,7 +140,7 @@
 #define HEATER_0_PIN       13
 #define HEATER_1_PIN       12
 #define HEATER_2_PIN       11
-#define HEATER_BED_PIN      7 // BED
+#define HEATER_BED_PIN      7
 
 #define FAN_PIN             9
 #define FAN1_PIN            8
